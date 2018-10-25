d6fd3b9ccf79519e6ae77f345dd0016ff7cb130d
andrew.zalewski <andrew.zalewski@bluestembrands.com>
2018-10-25 09:18:51 -0500

    [OMS-2739] Andrew - cleanup

--- src/main/groovy/com/bsb/fms/bsbfmsmockservice/Application.groovy
+++ src/main/groovy/com/bsb/fms/bsbfmsmockservice/Application.groovy
@@ -3,12 +3,16 @@ package com.bsb.fms.bsbfmsmockservice
 import com.github.tomakehurst.wiremock.core.Options
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import com.github.tomakehurst.wiremock.WireMockServer
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
+@SpringBootApplication
 class Application {
 
     static void main(String[] args) {
         Options options = new WireMockConfiguration().port(8089).usingFilesUnderClasspath("stubs")
         WireMockServer wireMockServer = new WireMockServer(options)
+        SpringApplication.run(Application.class, args)
         wireMockServer.start()
     }
 }
